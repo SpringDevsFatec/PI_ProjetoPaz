00391015e8acfa29d3df692945f9e5543e25ccb0
nasc2005 <murilovnasc.prog@gmail.com>
2025-06-07 07:11:36 -0300

    update_v2 Update Responses in Utils Change class statics to trait

--- Back-end/Controller/UserController.php
+++ Back-end/Controller/UserController.php
@@ -5,71 +5,69 @@ use App\Backend\Service\UserService;
 use App\Backend\Utils\Responses;
 
 class UserController {
+    
     private $service;
+
+    // Use the Responses trait 
+    use Responses;
 
     public function __construct() {
         $this->service = new UserService();
     }
 
-    // Login method to authenticate users
     public function login() {
         $data = json_decode(file_get_contents('php://input'));
 
-        if($result = $this->service->login($data)) {
-            Responses::send($result['status'], $result['message'], $result['content'], 200);
+        if ($result = $this->service->login($data)) {
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 200);
         } else {
-            Responses::send($result['status'], $result['message'], $result['content'], 401);
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 401);
         }
     }
 
-    // Methods to handle user operations
     public function getUserById() {
-        if($result = $this->service->getUserById()) {
-            Responses::send($result['status'], $result['message'], $result['content'], 200);
+        if ($result = $this->service->getUserById()) {
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 200);
         } else {
-            Responses::send($result['status'], $result['message'], $result['content'], 401);
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 401);
         }
     }
 
-    // Method to retrieve all users
     public function getAllUsers() {
-        if($result = $this->service->getAllUsers()) {
-            Responses::send($result['status'], $result['message'], $result['content'], 200);
+        if ($result = $this->service->getAllUsers()) {
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 200);
         } else {
-            Responses::send($result['status'], $result['message'], $result['content'], 401);
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 401);
         }
     }
 
-    // Method to create a new user
     public function createUser() {
         $data = json_decode(file_get_contents('php://input'));
 
-        if($result = $this->service->createUser($data)) {
-            Responses::send($result['status'], $result['message'], $result['content'], 201);
+        if ($result = $this->service->createUser($data)) {
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 201);
         } else {
-            Responses::send($result['status'], $result['message'], $result['content'], 404);
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 404);
         }
     }
 
-    // Method to update an existing user
     public function updateUser() {
         $data = json_decode(file_get_contents('php://input'));
 
-        if($result = $this->service->updateUser($data)) {
-            Responses::send($result['status'], $result['message'], $result['content'], 200);
+        if ($result = $this->service->updateUser($data)) {
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 200);
         } else {
-            Responses::send($result['status'], $result['message'], $result['content'], 404);
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 404);
         }
     }
 
-    // Method to upate a user's password
     public function updateUserPassword() {
         $data = json_decode(file_get_contents('php://input'));
 
-        if($result = $this->service->updateUserPassword($data)) {
-            Responses::send($result['status'], $result['message'], $result['content'], 200);
+        if ($result = $this->service->updateUserPassword($data)) {
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 200);
         } else {
-            Responses::send($result['status'], $result['message'], $result['content'], 404);
+            $this->handleResponse($result['status'], $result['message'], $result['content'], 404);
         }
     }
 }
--- Back-end/Utils/Responses.php
+++ Back-end/Utils/Responses.php
@@ -1,25 +1,17 @@
 <?php
 namespace App\Backend\Utils;
 
-class Responses
-{ 
-    public static function send($status, $message = "Operação concluída.", $content = null, $httpCode = 200)
-    {
-        http_response_code($httpCode);
+trait Responses {
+    
+    public function handleResponse($result, $successMessage = "Operação concluída com sucesso.", $content = null, $http_response_code = 200) {
+        http_response_code($http_response_code);
+
         echo json_encode([
-            "status" => $status,
-            "message" => $message,
+            "status" => $result,
+            "message" => $successMessage,
             "content" => $content
         ]);
     }
 
-    public static function success($message = "Sucesso!", $content = null, $httpCode = 200)
-    {
-        self::send(true, $message, $content, $httpCode);
-    }
-
-    public static function error($message = "Erro na operação.", $content = null, $httpCode = 400)
-    {
-        self::send(false, $message, $content, $httpCode);
-    }
+    
 }
