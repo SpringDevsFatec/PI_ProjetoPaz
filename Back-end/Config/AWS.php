85948f1343e2f829b067aae41eaa875451eff28c
nasc2005 <murilovnasc.prog@gmail.com>
2025-06-12 21:36:03 -0300

    Retired AWS Account

--- Back-end/Config/AWS.php
+++ Back-end/Config/AWS.php
@@ -7,16 +7,7 @@ class AWSAuth {
     const REGION = 'us-east-1';
     const VERSION = 'latest';
     const BUCKET = [
-        "Product" => [
-            "NAME" => "project-paz-product",
-            "ACESS_KEY_ID" => "AKIAS2VS4K2OFFYLV6JA/",
-            "SECRET_ACCESS_KEY" => "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"
-        ],
-        "Sale" => [
-            "NAME" => "project-paz-sale",
-            "ACESS_KEY_ID" => "AKIAS2VS4K2OFFYLV6JA/",
-            "SECRET_ACCESS_KEY" => "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"
-        ],
+        
     ];
     
 }
